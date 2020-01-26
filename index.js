ac68002139afa4e3faf60671f60b01206733c929
rsiajr <rsiajr@gmail.com>
2020-01-25 20:23:31 -0500

    2nd commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+
+const exphbs = require('express-handlebars'); 
+
+const app = express(); 
+
+app.engine('handlebars', exphbs()); 
+
+app.set('view engine', 'handlebars'); 
+
+app.get('/', function (req, res) { 
+
+    res.render('home'); 
+
+    res.send("Home Page"); 
+
+}); 
+
+app.listen(3000); 
