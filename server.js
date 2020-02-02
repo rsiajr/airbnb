31ced2988ab89bc3e77b2c6f6af4e1b74dcb1a48
rsiajr <rsiajr@gmail.com>
2020-02-01 19:16:03 -0500

    fixing room listing.

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ app.use(express.static('public'));
 
 //Request and Response
 
+const htmlTemplate = obj =>
+{
+    return `
+    <h1 class="search">Search Results</h1>
+    <div class="partition"></div>
+        <article class="productsbox">
+    <div class="partition"></div>
+
+    `;
+
+}
+
+
 app.get('/', function (req, res) { 
 
     res.render('main',{
@@ -66,25 +79,46 @@ app.get("/rooms",(req,res)=>{
 
     const prodDB= [];
 
-    prodDB.push({prodImg: 'img/forrent-1', prodTitle: 'Room for rent', prodDesc:'Sun-filled spacious single-bedroom room for rent', Rate:`649.99`});
+    prodDB.push({img:'img/forrent-1', title:'Room for rent', desc:'Sun-filled spacious single-bedroom room for rent', rate:`649.99`});
 
-    prodDB.push({prodImg: 'img/forrent-2', prodTitle: 'House for rent', prodDesc:'Tuscan-inspired house for rent', Rate:`749.99`});
+    prodDB.push({img:'img/forrent-2',title:'House for rent', desc:'Tuscan-inspired house for rent', rate:`749.99`});
 
-    prodDB.push({prodImg: 'img/forrent-3', prodTitle: 'Rooms for rent', prodDesc:'Chic 3-bedroom house for rent', Rate:`849.99`});
+    prodDB.push({img:'img/forrent-3',title:'Rooms for rent', desc:'Chic 3-bedroom house for rent', rate:`849.99`});
 
-    prodDB.push({prodImg: 'img/forrent-4', prodTitle: 'Tree house for rent', prodDesc:'Treehouse in the jungle for rent', Rate:`949.99`});
+    prodDB.push({img:'img/forrent-4',title:'Tree house for rent', desc:'Treehouse in the jungle for rent', rate:`949.99`});
 
-    prodDB.push({prodImg: 'img/forrent-5', prodTitle: 'Room for rent', prodDesc:'Quaint room in the city for rent', Rate:`1049.99`});
+    prodDB.push({img:'img/forrent-5', title:'Room for rent', desc:'Quaint room in the city for rent', rate:`1049.99`});
 
-    prodDB.push({prodImg: 'img/forrent-6', prodTitle: 'Room for rent', prodDesc:'Castle-like room for rent', Rate:`1149.99`});
+    prodDB.push({img:'img/forrent-6', title:'Room for rent', desc:'Castle-like room for rent', rate:`1149.99`});
 
+    let htmlSnippet=``;
 
-    res.render("roomlisting",{
-        title: "Rooms",
-        headingInfo : "Room Listing Page",
-        products : prodDB
+    for(let i=0; i<prodDB.length; i++)
+    {
+        htmlSnippet+=
+        ` <div>
+                <p>Product Title :${prodDB[i].img} </p>
+                <p>Price : ${prodDB[i].title} </p>
+                <p>Description : ${prodDB[i].description} </p>
+                <p>Description : ${prodDB[i].rate} </p>
+          </div>
+        `;
+    }
 
-    });
+    const data =
+    {
+
+        title: `Room Listing`,
+        headingInfo :`Room Listing`,
+        HTML:
+        `
+        <section>
+            ${htmlSnippet}
+        </section>`
+    };
+
+    res.send(htmlTemplate(data));
+
 });
 
 //Port creation
